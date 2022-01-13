a2139f2312650c9e6805191943a950bd5c9d401b
unknown <valentin7gubin@gmail.com>
2022-01-13 14:43:25 +0300

    first commit

---
+++ js/main-page-scripts.js
@@ -0,0 +1,176 @@
+// Слайдер на главном экране
+var swiperFirstScreen = new Swiper(".alloy-main-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".alloy-main-slider__next",
+    prevEl: ".alloy-main-slider__prev",
+  },
+});
+
+// Слайдер на странице "Нам Доверяют"
+const swiperTrustPage = new Swiper(".alloy-trust-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  width: 690,
+  draggable: true,
+  navigation: {
+    nextEl: ".alloy-trust-slider__next",
+    prevEl: ".alloy-trust-slider__prev",
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      width: 292,
+      height: 212,
+      slidesPerView: 1,
+      spaceBetween: 100,
+    },
+    375: {
+      width: 342,
+      height: 266,
+      slidesPerView: 1,
+    },
+    425: {
+      width: 392,
+      height: 316,
+    },
+    768: {
+      // width: 732,
+      // height: 616,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    },
+    1024: {
+      // width: 690,
+      // height: 584,
+    },
+  },
+});
+
+//Слайдеры на странице "Продукты"
+const swiperProductAdvantage = new Swiper(".alloy-advantage__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  draggable: true,
+  navigation: {
+    nextEl: ".alloy-advantage-slider__next",
+    prevEl: ".alloy-advantage-slider__prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiperProductWelding = new Swiper(".alloy-welding__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".alloy-product-slider-welding__next",
+    prevEl: ".alloy-product-slider-welding__prev",
+  },
+});
+const swiperProductParts = new Swiper(".alloy-parts__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".alloy-product-slider-parts__next",
+    prevEl: ".alloy-product-slider-parts__prev",
+  },
+});
+const swiperProductProduction = new Swiper(".alloy-production__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".alloy-product-slider-parts__next",
+    prevEl: ".alloy-product-slider-parts__prev",
+  },
+  draggable: true,
+});
+const swiperProductTrust = new Swiper(".alloy-trust__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".alloy-product-slider-trust__next",
+    prevEl: ".alloy-product-slider-trust__prev",
+  },
+  draggable: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+// Табы в разделе "Каталог"
+
+const tab = document.querySelectorAll(".tab");
+console.log(tab);
+const onClickTabNavLink = () => {
+  Array.prototype.forEach.call(tab, (e) => {
+    const tabNavLinks = e.querySelectorAll(".tab-nav-link");
+
+    Array.prototype.forEach.call(tabNavLinks, (e, tabIndex) => {
+      e.addEventListener(
+        "click",
+        (event) => {
+          const tabPanes = e
+            .closest(".tab")
+            .querySelector(".tab-body").children;
+          const tabNavItems = e
+            .closest(".tab")
+            .querySelector(".tab-nav")
+            .querySelectorAll(".tab-nav-item");
+
+          event.preventDefault();
+
+          Array.prototype.forEach.call(tabNavItems, (e, indexNav) => {
+            if (indexNav === tabIndex) {
+              e.classList.add("active");
+            } else {
+              e.classList.remove("active");
+            }
+          });
+
+          Array.prototype.forEach.call(tabPanes, (e, indexPane) => {
+            if (indexPane === tabIndex) {
+              e.classList.add("active");
+            } else {
+              e.classList.remove("active");
+            }
+          });
+        },
+        false
+      );
+    });
+  });
+};
+
+// Init
+const init = () => {
+  // Tabs Init
+  onClickTabNavLink();
+};
+
+document.addEventListener("DOMContentLoaded", init);
---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Открытие / скрытие мобильного меню
+
+$(document).ready(function(){
+    $('.alloy-header__toggler').on('click', function(){
+        let toggle = false
+
+        $('.alloy-header__nav').toggleClass('alloy-header__nav-active')
+
+        $('.alloy-header__nav').hasClass('alloy-header__nav-active') ? toggle = true : toggle = false
+
+        if(toggle){
+            $('.alloy-open').removeClass('alloy-active')
+            $('.alloy-close').addClass('alloy-active')
+        } else {
+            $('.alloy-open').addClass('alloy-active')
+            $('.alloy-close').removeClass('alloy-active')
+        }
+    })
+})
+
+// Раскрытие поиска
+
+$(document).ready(function(){
+    $('.alloy-header__search a').on('click', function(){
+        $('.alloy-header__search-block').toggleClass('alloy-header__search-block_active')
+    })
+
+    $('.alloy-header__search-block svg').on('click', function(){
+        $(this).parents('.alloy-header__search-block').attr('class', 'alloy-header__search-block')
+    })
+})
+
+
+// Раскрытие панели смены языков
+
+$(document).ready(function(){
+    $('.alloy-header__lang').on('click', function(){
+        $('.alloy-header__lang-block').toggleClass('alloy-header__lang-block_active')
+    })
+
+    $('.alloy-header__lang-block span').each(function(){
+        $(this).on('click', function(){
+            $('.alloy-header__lang-main').text($(this).text())
+            setTimeout(()=>{
+                $('.alloy-header__lang-block').removeClass('alloy-header__lang-block_active')
+            },500)
+        })
+    })
+
+})
+
